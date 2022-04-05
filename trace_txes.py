caa7ff648f44b7b202232bf129295354334de504
charros96 <88594194+charros96@users.noreply.github.com>
2022-04-05 12:58:05 -0700

    Add files via upload

--- trace_txes.py
+++ trace_txes.py
@@ -53,18 +53,19 @@ class TXO:
         inputs = tx.get('vin')
         print(tx)
         print(inputs)
+        print(rpc_connection.getrawtransaction(inputs[0].get('txid'),True)
         """
-        for depth in range(d):
-            for txo in inputs:
-
-                for i in range(len(inputs)):
-                    txo = tx.get('vin')[i]
-                    print(txo)
-                    amount = int(txo.get('value')*pow(10,8))
-                    owner = txo.get('scriptPubKey').get('hex')
-                    time = datetime.fromtimestamp(tx.get('time'))
-                    self.inputs.append(TXO(self.tx_hash,i,amount,owner,time))
-            inputs = t
+        for input in inputs:
+            tx = rpc_connection.getrawtransaction(input.get(txid),True)    
+            print(tx)
+            for i in range(len(inputs)):
+                txo = tx.get('vin')[i]
+                print(txo)
+                amount = int(txo.get('value')*pow(10,8))
+                owner = txo.get('scriptPubKey').get('hex')
+                time = datetime.fromtimestamp(tx.get('time'))
+                self.inputs.append(TXO(self.tx_hash,i,amount,owner,time))
+            
         """
         pass
         #YOUR CODE HERE
