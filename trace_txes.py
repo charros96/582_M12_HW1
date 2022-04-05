c3425a09a16c0e91a104cdc966d2b3b7b2001958
charros96 <88594194+charros96@users.noreply.github.com>
2022-04-05 16:58:48 -0700

    Add files via upload

--- trace_txes.py
+++ trace_txes.py
@@ -57,7 +57,7 @@ class TXO:
         #print(parent)
         #print("tx.vin.txid.vout")
         tx_get_inputs = TXO.from_tx_hash('1620c59574743195fb5ad0d0bf96ac4e16a78f3912a58d23c6e2aeaf2595bfe7')
-        print(tx_get_inputs)
+        print(rpc_connection.getrawtransaction(tx_get_inputs,True))
         vin_outputs = rpc_connection.getrawtransaction(parent_id,True).get('vout')
         for i in range(len(vin_outputs)):
             self.inputs.append(TXO.from_tx_hash(parent_id,i))
